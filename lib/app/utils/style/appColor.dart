38ac21c8b8db06399bccbb376f98b0763c6fe821
penaa9 <trivena.natalia2015@gmail.com>
2022-07-05 03:02:31 +0700

    style untuk warna

---
+++ lib/app/utils/style/appColor.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+
+class appColor{
+  static const primaryBg = Color(0xFFBFE2FF);
+  static const primaryText = Color.fromARGB(255, 128, 126, 126);
+  static const iconWarna = Colors.grey;
+
+
+
+}
