b485ce9f7d77b5903b0724742a1075c291adef10
penaa9 <trivena.natalia2015@gmail.com>
2022-07-05 03:05:59 +0700

    merubah warna text dan Background Color

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Venavenven Task App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
