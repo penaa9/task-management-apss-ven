4c29f6c847e05f09ad59f09cb8c019ea5a389628
penaa9 <trivena.natalia2015@gmail.com>
2022-07-13 04:36:47 +0700

    video 9 friend view

--- lib/app/modules/friend/views/friend_view.dart
+++ lib/app/modules/friend/views/friend_view.dart
@@ -1,8 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../../../utils/widget/headers.dart';
+import '../../../utils/widget/myFriendss.dart';
 import '../../../utils/widget/sidebar.dart';
 import '../controllers/friend_controller.dart';
 import 'package:management_taks_apps/app/utils/style/appColor.dart';
@@ -35,16 +38,13 @@ class FriendView extends GetView<FriendController> {
                         child: Row(children: [
                           IconButton(
                             onPressed: () {
-
-                            _drawwerKey.currentState!.openDrawer();
-                            
+                              _drawwerKey.currentState!.openDrawer();
                             },
                             icon: Icon(
                               Icons.menu,
                               color: appColor.primaryText,
                             ),
                           ),
-
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: const [
@@ -59,32 +59,108 @@ class FriendView extends GetView<FriendController> {
                                       color: appColor.primaryText))
                             ],
                           ),
-                          const SizedBox(width: 45,),
+                          const SizedBox(
+                            width: 45,
+                          ),
                           const Icon(Icons.notifications,
                               color: appColor.primaryText),
-                          const SizedBox(width: 15,),
-
-
-                              ClipRRect(
-                              borderRadius: BorderRadius.circular(30),
-                              child: CircleAvatar(backgroundColor: Colors.amberAccent, radius: 25, foregroundImage: NetworkImage("https://th.bing.com/th/id/R.01eb473c2e847284db9d7ccfb711b6de?rik=Dam3wkV8SszROw&riu=http%3a%2f%2fd263ao8qih4miy.cloudfront.net%2fwp-content%2fuploads%2f2017%2f06%2fSuspiciousPartner29-30-00282.jpg&ehk=9CVjvndW6EBhxZ58qoE%2fiUGyBo6pSWzvMSl93Qz38Vs%3d&risl=&pid=ImgRaw&r=0"),),
-                              )
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.amberAccent,
+                              radius: 25,
+                              foregroundImage: NetworkImage(
+                                  "https://th.bing.com/th/id/R.01eb473c2e847284db9d7ccfb711b6de?rik=Dam3wkV8SszROw&riu=http%3a%2f%2fd263ao8qih4miy.cloudfront.net%2fwp-content%2fuploads%2f2017%2f06%2fSuspiciousPartner29-30-00282.jpg&ehk=9CVjvndW6EBhxZ58qoE%2fiUGyBo6pSWzvMSl93Qz38Vs%3d&risl=&pid=ImgRaw&r=0"),
+                            ),
+                          )
                         ]),
                       ),
 
-
-                  //content isi page /screen
+                //content isi page /screen
                 Expanded(
                     child: Container(
-                  padding: const EdgeInsets.all(50),
+                  padding: !context.isPhone
+                      ? const EdgeInsets.all(50)
+                      : const EdgeInsets.all(20),
                   margin: !context.isPhone
                       ? const EdgeInsets.all(10)
                       : const EdgeInsets.all(0),
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: !context.isPhone
-                          ? BorderRadius.circular(50)
-                          : BorderRadius.circular(20)),
+                    color: Colors.white,
+                    borderRadius: !context.isPhone
+                        ? BorderRadius.circular(50)
+                        : BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //widget 1
+                      const Text(
+                        'People you May Now',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: appColor.primaryText),
+                      ),
+                      SizedBox(
+                        height: 200,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: 6,
+                          clipBehavior: Clip.antiAlias,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: const Image(
+                                      image: NetworkImage(
+                                          'https://th.bing.com/th/id/R.01eb473c2e847284db9d7ccfb711b6de?rik=Dam3wkV8SszROw&riu=http%3a%2f%2fd263ao8qih4miy.cloudfront.net%2fwp-content%2fuploads%2f2017%2f06%2fSuspiciousPartner29-30-00282.jpg&ehk=9CVjvndW6EBhxZ58qoE%2fiUGyBo6pSWzvMSl93Qz38Vs%3d&risl=&pid=ImgRaw&r=0'),
+                                    ),
+                                  ),
+                                  const Positioned(
+                                      bottom: 10,
+                                      left: 100,
+                                      child: Text(
+                                        "Nam Jha Hyun",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      )),
+                                  Positioned(
+                                      bottom: 0,
+                                      right: 0,
+                                      child: SizedBox(
+                                        height: 36,
+                                        width: 36,
+                                        child: ElevatedButton(
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                              padding: EdgeInsets.zero,
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          50))),
+                                          child: Icon(Icons
+                                              .add_circle_outline_outlined),
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+
+                      MyFriends(),
+                    ],
+                  ),
                 ))
               ],
             ),
