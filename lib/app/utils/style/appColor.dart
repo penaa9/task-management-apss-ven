2916d20daae8d5273abb0c42def8eaf1ff82df15
penaa9 <trivena.natalia2015@gmail.com>
2022-07-13 05:15:08 +0700

    background appcolor update

--- lib/app/utils/style/appColor.dart
+++ lib/app/utils/style/appColor.dart
@@ -3,7 +3,7 @@ import 'package:matcher/matcher.dart';
 
 class appColor{
   static const primaryBg = Color(0xFFBFE2FF);
-  static const secondaryBg = Color.fromARGB(255, 188, 188, 188);
+  static const secondaryBg = Color.fromARGB(255, 243, 243, 243);
   static const primaryText = Color.fromARGB(255, 128, 126, 126);
   static const iconWarna = Colors.grey;
 
