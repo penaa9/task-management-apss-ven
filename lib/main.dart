47bd14d93928e3c2976c303525a19023ace8f5a4
penaa9 <trivena.natalia2015@gmail.com>
2022-07-19 04:03:57 +0700

    commit video 12

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  //untuk menjalankan diberbagai device tertentu
+
   await Firebase.initializeApp(
-    
+     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
     GetMaterialApp(
