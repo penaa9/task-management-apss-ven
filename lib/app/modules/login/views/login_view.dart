34051bba5b3c4ca883523030303737bffed20f6f
penaa9 <trivena.natalia2015@gmail.com>
2022-08-11 06:51:13 +0700

    test

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,13 +1,15 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:management_taks_apps/app/data/controller/auth_controller.dart';
 
-import '../../../data/controller/auth_controller.dart';
 import '../controllers/login_controller.dart';
+
+// import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   final authC = Get.find<AuthController>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,19 +25,19 @@ class LoginView extends GetView<LoginController> {
             !context.isPhone? 
             Expanded(
                 child: Container(
-                  padding: EdgeInsets.all(34),
+                  padding: EdgeInsets.all(12),
              decoration: BoxDecoration(
                color: Colors.blue,
-              borderRadius: BorderRadius.circular(50)
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(50), bottomLeft: Radius.circular(50))
              ),
               
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Selamat Datang", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
-                  Text("di App Task Management", style: TextStyle(color: Colors.white, fontSize: 16),),
-                  Text("Yuk Login or Signup", style: TextStyle(color: Colors.white, fontSize: 16),),
+                  Text("HI, UCIC", style: TextStyle(color: Colors.white, fontSize: 30),),
+                  Text("Selamat Datang", style: TextStyle(color: Colors.white, fontSize: 18),),
+                  Text("Yuk Login", style: TextStyle(color: Colors.white, fontSize: 18),),
                 ],
               ),
             )
@@ -44,9 +46,9 @@ class LoginView extends GetView<LoginController> {
                 child: Container(
              decoration: BoxDecoration(
                color: Colors.white,
-              borderRadius: BorderRadius.circular(50)
+            borderRadius: BorderRadius.only(topRight: Radius.circular(50), bottomRight: Radius.circular(50), topLeft: Radius.circular(50), bottomLeft: Radius.circular(50)),
 
-             ),
+          ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -54,24 +56,17 @@ class LoginView extends GetView<LoginController> {
                   Image.asset("assets/image/login.png", height: 400,),
                   Container(
                     padding: EdgeInsets.all(12),
-                    width: 120,
                     decoration: BoxDecoration(
                       color: Colors.blue,
                       borderRadius: BorderRadius.circular(12)
                     ),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                          authC.signInWithGoogle();
+                    child: InkWell(
+                      onTap: (){
+                       authC.signInWithGoogle();
                       },
                       child: Ink(
                         
-                        child: Row(
-                        children: [
-                       Icon(Icons.arrow_circle_right_sharp),
-                        Text("Sign Up",style: TextStyle(color: Colors.white, fontSize: 12 ),),
-                        ],
-                        
-                        ),
+                        child: Text("Sign In With Google",style: TextStyle(color: Colors.white, fontSize: 12 ),),
                       ),
                     ),
                   )
