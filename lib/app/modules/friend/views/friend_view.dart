bbf911b8163c0ea114db768508095ec18740c877
penaa9 <trivena.natalia2015@gmail.com>
2022-08-11 08:22:27 +0700

    commit vid 15

--- lib/app/modules/friend/views/friend_view.dart
+++ lib/app/modules/friend/views/friend_view.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:management_taks_apps/app/data/controller/auth_controller.dart';
 
 import '../../../utils/widget/headers.dart';
 import '../../../utils/widget/myFriendss.dart';
@@ -12,161 +13,181 @@ import 'package:management_taks_apps/app/utils/style/appColor.dart';
 
 class FriendView extends GetView<FriendController> {
   final GlobalKey<ScaffoldState> _drawwerKey = GlobalKey();
+  final authCon = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _drawwerKey,
-      drawer: const SideBar(),
+      drawer: const SizedBox(width: 150, child: SideBar()),
       backgroundColor: appColor.primaryBg,
-      body: Row(
-        children: [
-          !context.isPhone
-              ? const Expanded(
-                  flex: 2,
-                  //lebih dari 600 px, responsive
-                  child: const SideBar(),
-                )
-              : SizedBox(),
-          Expanded(
-            flex: 15,
-            child: Column(
-              children: [
-                !context.isPhone
-                    ? const Header()
-                    : Container(
-                        padding: EdgeInsets.all(20),
-                        child: Row(children: [
-                          IconButton(
-                            onPressed: () {
-                              _drawwerKey.currentState!.openDrawer();
-                            },
-                            icon: Icon(
-                              Icons.menu,
-                              color: appColor.primaryText,
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                'Task Management',
-                                style: TextStyle(
-                                    fontSize: 20, color: appColor.primaryText),
-                              ),
-                              Text('Menjadi mudah dengan Task Management',
-                                  style: TextStyle(
-                                      fontSize: 12,
-                                      color: appColor.primaryText))
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 45,
-                          ),
-                          const Icon(Icons.notifications,
-                              color: appColor.primaryText),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(30),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.amberAccent,
-                              radius: 25,
-                              foregroundImage: NetworkImage(
-                                  "https://th.bing.com/th/id/R.01eb473c2e847284db9d7ccfb711b6de?rik=Dam3wkV8SszROw&riu=http%3a%2f%2fd263ao8qih4miy.cloudfront.net%2fwp-content%2fuploads%2f2017%2f06%2fSuspiciousPartner29-30-00282.jpg&ehk=9CVjvndW6EBhxZ58qoE%2fiUGyBo6pSWzvMSl93Qz38Vs%3d&risl=&pid=ImgRaw&r=0"),
-                            ),
-                          )
-                        ]),
-                      ),
-
-                //content isi page /screen
-                Expanded(
-                    child: Container(
-                  padding: !context.isPhone
-                      ? const EdgeInsets.all(50)
-                      : const EdgeInsets.all(20),
-                  margin: !context.isPhone
-                      ? const EdgeInsets.all(10)
-                      : const EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: !context.isPhone
-                        ? BorderRadius.circular(50)
-                        : BorderRadius.circular(30),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //widget 1
-                      const Text(
-                        'People you May Now',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: appColor.primaryText),
-                      ),
-                      SizedBox(
-                        height: 200,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemCount: 6,
-                          clipBehavior: Clip.antiAlias,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Stack(
+      body: SafeArea(
+        child: Row(
+          children: [
+            !context.isPhone
+                ? const Expanded(
+                    flex: 2,
+                    child: const SideBar(),
+                  )
+                : const SizedBox(),
+            Expanded(
+              flex: 15,
+              child: Column(
+                children: [
+                  !context.isPhone
+                      ? const Header()
+                      : Container(
+                          padding: EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              Column(
                                 children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(50),
-                                    child: const Image(
-                                      image: NetworkImage(
-                                          'https://th.bing.com/th/id/R.01eb473c2e847284db9d7ccfb711b6de?rik=Dam3wkV8SszROw&riu=http%3a%2f%2fd263ao8qih4miy.cloudfront.net%2fwp-content%2fuploads%2f2017%2f06%2fSuspiciousPartner29-30-00282.jpg&ehk=9CVjvndW6EBhxZ58qoE%2fiUGyBo6pSWzvMSl93Qz38Vs%3d&risl=&pid=ImgRaw&r=0'),
-                                    ),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            _drawwerKey.currentState!
+                                                .openDrawer();
+                                          },
+                                          icon: Icon(Icons.menu,
+                                              color: appColor.primaryText)),
+                                      const SizedBox(
+                                        width: 7,
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: const [
+                                          Text(
+                                            'Task Management',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                color: appColor.primaryText),
+                                          ),
+                                          Text(
+                                              'Menjadi mudah dengan Task Management',
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  color: appColor.primaryText))
+                                        ],
+                                      ),
+                                      const Spacer(),
+                                      const Icon(
+                                        Icons.notifications,
+                                        color: appColor.primaryText,
+                                        size: 30,
+                                      ),
+                                      const SizedBox(
+                                        width: 15,
+                                      ),
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(30),
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.amber,
+                                          radius: 25,
+                                          foregroundImage: NetworkImage(authCon
+                                              .auth.currentUser!.photoURL!),
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                  const Positioned(
-                                      bottom: 10,
-                                      left: 100,
-                                      child: Text(
-                                        "Nam Jha Hyun",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      )),
-                                  Positioned(
-                                      bottom: 0,
-                                      right: 0,
-                                      child: SizedBox(
-                                        height: 36,
-                                        width: 36,
-                                        child: ElevatedButton(
-                                          onPressed: () {},
-                                          style: ElevatedButton.styleFrom(
-                                              padding: EdgeInsets.zero,
-                                              shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          50))),
-                                          child: Icon(Icons
-                                              .add_circle_outline_outlined),
-                                        ),
-                                      ))
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  // inputan serach
+                                  context.isPhone
+                                      ? TextField(
+                                          //akasi
+                                          onChanged: (value) =>
+                                              authCon.searchFriends(value),
+                                          controller:
+                                              authCon.searchFriendsController,
+                                          decoration: InputDecoration(
+                                            filled: true,
+                                            fillColor: Colors.white,
+                                            contentPadding:
+                                                const EdgeInsets.only(
+                                                    left: 40, right: 10),
+                                            enabledBorder: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                borderSide: const BorderSide(
+                                                    color: Colors.white)),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                              borderSide: const BorderSide(
+                                                  color: Colors.blue),
+                                            ),
+                                            prefixIcon:
+                                                const Icon(Icons.search),
+                                            hintText: 'Cari Disini',
+                                          ),
+                                        )
+                                      : const SizedBox(),
                                 ],
                               ),
-                            );
-                          },
+                            ],
+                          ),
                         ),
+                  //content / isipage /screen
+                  Expanded(
+                    child: Container(
+                      padding: !context.isPhone
+                          ? const EdgeInsets.all(30)
+                          : const EdgeInsets.all(10),
+                      margin:
+                          !context.isPhone ? const EdgeInsets.all(10) : null,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: !context.isPhone
+                              ? BorderRadius.circular(50)
+                              : BorderRadius.circular(30)),
+                      child: Obx(
+                        () => authCon.hasilPencarian.isEmpty
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                    const Text(
+                                      'People You May Know',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: appColor.primaryText,
+                                      ),
+                                    ),
+                                    peopleYouMayKnow(),
+                                    myFriends(),
+                                  ])
+                            : ListView.builder(
+                                padding: EdgeInsets.all(5),
+                                shrinkWrap: true,
+                                itemCount: authCon.hasilPencarian.length,
+                                itemBuilder: (context, index) => ListTile(
+                                  onTap: () => authCon.addFriends(
+                                      authCon.hasilPencarian[index]['email']),
+                                  leading: ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image(
+                                      image: NetworkImage(authCon
+                                          .hasilPencarian[index]['photo']),
+                                    ),
+                                  ),
+                                  title: Text(
+                                      authCon.hasilPencarian[index]['name']),
+                                  subtitle: Text(
+                                      authCon.hasilPencarian[index]['email']),
+                                  trailing: Icon(Icons.add),
+                                ),
+                              ),
                       ),
-
-                      MyFriends(),
-                    ],
+                    ),
                   ),
-                ))
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
+ }
 }
