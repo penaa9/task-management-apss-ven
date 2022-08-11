78265a472843c8f44448ae2045e38f3b0f1f1863
penaa9 <trivena.natalia2015@gmail.com>
2022-08-11 07:10:24 +0700

    commit video 14

--- lib/app/data/controller/auth_controller.dart
+++ lib/app/data/controller/auth_controller.dart
@@ -1,31 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:management_taks_apps/app/routes/app_pages.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 
 class AuthController extends GetxController {
+ FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
   Future signInWithGoogle() async {
     // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  UserCredential? _userCredential;
 
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
-
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-
     print(googleUser!.email);
     // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance
+    await auth
         .signInWithCredential(credential)
-        .then((value) => Get.offAllNamed(Routes.HOME));
+        .then((value) => _userCredential = value);
+
+    print(googleUser.email);
+
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance
+        .signInWithCredential(credential)
+        .then((value) => _userCredential = value);
+
+    CollectionReference users = firestore.collection('users');
+
+    final cekUser = await users.doc(googleUser.email).get();
+    if (!cekUser.exists) {
+      users.doc(googleUser.email).set({
+        'uid': _userCredential!.user!.uid,
+        'name': googleUser.displayName,
+        'email': googleUser.email,
+        'photo': googleUser.photoUrl,
+        'createdAt': _userCredential!.user!.metadata.creationTime.toString(),
+        'lastLoginAt':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+        // 'list_cari' : [R, RE, REZ, REZA]
+      }).then((value) async {
+        String temp = '';
+        try {
+          for (var i = 0; 1 < googleUser.displayName!.length; i++) {
+            temp = temp + googleUser.displayName![i];
+            await users.doc(googleUser.email).set({
+              'list_cari': FieldValue.arrayUnion([temp.toUpperCase()])
+            }, SetOptions(merge: true));
+          }
+        } catch (e) {
+          print(e);
+        }
+      });
+    } else {
+      users.doc(googleUser.email).update({
+        'lastLoginAt':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+      });
+    }
+    Get.offAllNamed(Routes.HOME);
   }
+
+ 
 
   Future logout() async {
     await FirebaseAuth.instance.signOut();
